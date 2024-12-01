aaf2745756d7c903476d657b375606ed64e15c6c
jameskimani001 <jameskim092t@gmail.com>
2024-12-01 14:13:49 +0300

    function decorators complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,39 +1,35 @@
 APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+    "Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle",
+    "French Bulldog", "Pug", "Pointer"
 ]
-
 
 class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
-        self.name = name
-        self.breed = breed
+        self.name = name  # This will trigger the setter for name
+        self.breed = breed  # This will trigger the setter for breed
 
-    def get_name(self):
+    @property
+    def name(self):
+        """The name property"""
         return self._name
 
-    def set_name(self, name):
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name.title()  # Title-case the name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    @property
+    def breed(self):
+        """The breed property"""
         return self._breed
 
-    def set_breed(self, breed):
+    @breed.setter
+    def breed(self, breed):
+        """Breed must be in the list of approved breeds"""
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
             raise ValueError("Breed must be in list of approved breeds.")
-
-    breed = property(get_breed, set_breed)
